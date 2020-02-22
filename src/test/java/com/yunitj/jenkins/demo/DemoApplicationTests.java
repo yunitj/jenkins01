7f5f7d87e346beaae1c61142a989838a1f5dde80
cn3085 <cn3085@gmail.com>
2020-02-22 16:00:20 +0900

    init

---
+++ src/test/java/com/yunitj/jenkins/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunitj.jenkins.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
